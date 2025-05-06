b0d0245491a4498dbbac293f3a0bbc25c473d3ae
Telly Sow <bsow2021@gmail.com>
2025-05-06 18:59:49 -0400

    Second Changes

--- hacker-stories/src/components/Footer.jsx
+++ hacker-stories/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-gray-400 py-5 px-4 w-full  bottom-0 mt-8">
-      Created by Telly ®
+      Created by Telly 1®
     </footer>
   );
 }
